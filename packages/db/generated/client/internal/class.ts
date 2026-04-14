8a000e5b6a9004bcb2ac83e7645fd8bb8c63f28f
Mike <devlivelymike@gmail.com>
2026-04-14 12:47:50 -0700

    minor fix

--- packages/db/generated/client/internal/class.ts
+++ packages/db/generated/client/internal/class.ts
@@ -22,7 +22,7 @@ const config: runtime.GetPrismaClientConfig = {
       "value": "prisma-client"
     },
     "output": {
-      "value": "/Users/dev/Desktop/assess/kalos-assessment/packages/db/generated/client",
+      "value": "/Users/dev/Desktop/assess 2/kalos-assessment/packages/db/generated/client",
       "fromEnvVar": null
     },
     "config": {
@@ -36,7 +36,7 @@ const config: runtime.GetPrismaClientConfig = {
       }
     ],
     "previewFeatures": [],
-    "sourceFilePath": "/Users/dev/Desktop/assess/kalos-assessment/packages/db/prisma/schema.prisma",
+    "sourceFilePath": "/Users/dev/Desktop/assess 2/kalos-assessment/packages/db/prisma/schema.prisma",
     "isCustomOutput": true
   },
   "relativePath": "../../prisma",
